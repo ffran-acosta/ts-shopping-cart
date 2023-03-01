9d5cf7d98c6ff3851c50098902b1375977e2e558
Franco Acosta <francoaugustoacosta@gmail.com>
2023-03-01 09:29:55 -0300

    restore

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
       <Container className="mb-4"> 
         <Routes>
           {/* <Route path="/" element={<Home />}/> */}
-          <Route path="/ts-shopping-cart/store" element={<Store />} />
+          <Route path="/store" element={<Store />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </Container>
