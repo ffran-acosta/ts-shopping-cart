e8106baa982cacd51e96b9da30b737915d39acb2
Franco Acosta <francoaugustoacosta@gmail.com>
2023-03-07 16:01:31 -0300

    about

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,8 +1,10 @@
+import "./style.css"
 const About = () => {
 
     return (
-        <div>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta, officia illo repudiandae atque necessitatibus amet tempora iure similique deserunt doloribus dolorum nesciunt nihil, inventore animi. Dolorem est blanditiis nisi natus.
+        <div className="style">
+            The project was created to showcase my knowledge of TypeScript and React. It's an ongoing project that will become a more comprehensive and content-rich website. The aim is to create a platform that leverages the power of these technologies to deliver an exceptional user experience. The project demonstrates my skills in building modern web applications and my ability to work with these cutting-edge technologies. With every passing day, the project grows more ambitious, with new features and functionality being added regularly. The end result will be a website that delivers an immersive experience for users, providing them with an intuitive interface and access to an extensive library of content. Overall, this project is a testament to my passion for web development and my commitment to delivering high-quality, scalable solutions that meet the needs of modern businesses and consumers alike.
+
         </div>
     )
 }
