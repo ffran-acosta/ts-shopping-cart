06c672da6109efd048d847251006e860bb8a2f06
Franco Acosta <francoaugustoacosta@gmail.com>
2023-03-07 14:17:33 -0300

    cart local db to postgress

--- src/components/Navbar/Cart/Cart.tsx
+++ src/components/Navbar/Cart/Cart.tsx
@@ -3,13 +3,27 @@ import { formatCurrency } from "@/utilities"
 import { Offcanvas, OffcanvasBody, OffcanvasTitle, Stack } from "react-bootstrap"
 import { ShowItem } from "./CartItem"
 import storeItems from "@/data/items.json";
+import { StoreItemProps } from "@/models";
+import { testingService } from "@/services";
+import { useState, useEffect } from "react";
 
 type ShoppingCartProps = {
     isOpen: boolean
 }
 
 export const Cart = ({isOpen}: ShoppingCartProps) => {
+
+    const [products, setProducts] = useState<StoreItemProps[]>([])
+    const fetchProducts = async () => {
+        const { data } = await testingService()
+        setProducts(data)
+    }
+    useEffect(() => {
+        fetchProducts()
+    }, [])
+
     const {closeCart, cartItems} = useShoppingCart()
+    
     return (
         <Offcanvas        
             placement='end'
@@ -28,7 +42,7 @@ export const Cart = ({isOpen}: ShoppingCartProps) => {
                         Total{" "}
                         {formatCurrency(
                             cartItems.reduce((total, cartItem) => {
-                                const item = storeItems.find(i => i.id === cartItem.id)
+                                const item = products.find(i => i.id === cartItem.id)
                                 return total + (item?.price || 0) * cartItem.quantity
                             }, 0)
                         )}
--- src/components/Navbar/Cart/CartItem.tsx
+++ src/components/Navbar/Cart/CartItem.tsx
@@ -1,18 +1,30 @@
 import { useShoppingCart } from "@/context";
-import { CartItem } from "@/models";
-import storeItems from "@/data/items.json";
+import { CartItem, StoreItemProps } from "@/models";
+// import storeItems from "@/data/items.json";
+import { testingService } from "@/services";
 import { Button, Stack } from "react-bootstrap";
 import { formatCurrency } from "@/utilities";
+import { useEffect, useState } from "react";
 
 
 export const ShowItem= ({id, quantity}: CartItem) => {
+
+    const [products, setProducts] = useState<StoreItemProps[]>([])
+    const fetchProducts = async () => {
+        const { data } = await testingService()
+        setProducts(data)
+    }
+    useEffect(() => {
+        fetchProducts()
+    }, [])
+
     const {removeFromCart} = useShoppingCart();
-    const item = storeItems.find(item => item.id === id)
+    const item = products.find(item => item.id === id)
     if (item == null) return null;
     return (
         <Stack direction="horizontal" gap={2}>
             <img 
-                src={item.imgUrl} 
+                src={item.imgurl} 
                 style={{width:'75px', height:'150px', 
                 objectFit:'cover'}} />
             
